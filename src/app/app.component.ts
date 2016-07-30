fd4ca151afa2c6fe85311c5114d193f329cc1c9f
Stephen Sullivan <sullivankstephen@gmail.com>
2016-07-30 15:19:01 -0500

    Dockerized app w/ nginx proxy for backend requests.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import {Http, HTTP_PROVIDERS} from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+ moduleId: module.id,
+ selector: 'app-root',
+ templateUrl: 'app.component.html',
+ styleUrls: ['app.component.css'],
+ viewProviders: [HTTP_PROVIDERS]
 })
 export class AppComponent {
-  title = 'app works!!';
+ constructor(http: Http) {
+   var people;
+   http.get('/api/users/9879')
+   .toPromise().then(function(yik){console.log(yik)},function(yak){console.log(yak)});
+ }
 }
+
